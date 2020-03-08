3d727602938c4eedf674d05aeda2c8a66a25e488
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-08 14:22:46 -0400

    Added test case for ylabel

--- lib/matplotlib/tests/test_fix16389.py
+++ lib/matplotlib/tests/test_fix16389.py
@@ -16,10 +16,18 @@ def test_kwargs_order():
     assert fp_first.get_size() == 20
     assert size_first.get_size() == 20
 
-def test_if_not_set_back_to_default():
+def test_if_not_set_back_to_default_xlabel():
     #Test that setting fontproperties doesn't change the other properties back to default
     data = np.random.randn(100)
     plt.hist(data, bins=40, facecolor="red", edgecolor="blue", alpha=0.6)
     original = plt.xlabel("value", fontproperties="Comic Sans MS", weight="bold", size=16)
     changed = plt.xlabel("value", weight="bold", size=16, fontproperties="Comic Sans MS")
+    assert (original.get_size() == changed.get_size()) and (original.get_weight() == changed.get_weight()) and (original.get_family() == changed.get_family())
+
+def test_if_not_set_back_to_default_ylabell():
+    #Test that setting fontproperties doesn't change the other properties back to default
+    data = np.random.randn(100)
+    plt.hist(data, bins=40, facecolor="red", edgecolor="blue", alpha=0.6)
+    original = plt.ylabell("value", fontproperties="Comic Sans MS", weight="bold", size=16)
+    changed = plt.ylabell("value", weight="bold", size=16, fontproperties="Comic Sans MS")
     assert (original.get_size() == changed.get_size()) and (original.get_weight() == changed.get_weight()) and (original.get_family() == changed.get_family())
