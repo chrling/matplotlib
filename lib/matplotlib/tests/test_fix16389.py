fb156afdbc2e4e37c8cc23342e992e890864e884
Faris <faris.ally@mail.utoronto.ca>
2020-03-09 14:34:34 -0400

    Formatted and added acceptance test

--- lib/matplotlib/tests/test_fix16389.py
+++ lib/matplotlib/tests/test_fix16389.py
@@ -4,30 +4,50 @@ import numpy as np
 
 
 def test_kwargs_order():
+    '''
+    The issue was that setting Text properties and then setting the fontproperties
+    would overwrite the original Text properties. For example,
+    plt.xlabel("value", fontproperties='SimHei',size=20) <- This would set size correctly
+    plt.ylabel("counts",size=20, fontproperties='SimHei') <- This would not
+    
+    These tests ensure that this issue was fixed
+    '''
+    
     #General case
-    a = txt.Text(size=20, fontproperties="SimHei") 
-    assert a.get_size() == 20
+    text = txt.Text(size=20, fontproperties="SimHei") 
+    assert text.get_size() == 20
+    
+    #Data for tests
+    data = np.random.randn(10000)
     
     #Case from bug report
-    data = np.random.randn(10000)
     plt.hist(data, bins=40, facecolor="blue", edgecolor="black", alpha=0.5)
     fp_first = plt.xlabel("value", fontproperties='SimHei',size=20)
     size_first = plt.ylabel("counts",size=20, fontproperties='SimHei')
     assert fp_first.get_size() == 20
     assert size_first.get_size() == 20
-
-def test_if_not_set_back_to_default_xlabel():
-    #Test that setting fontproperties doesn't change the other properties back to default
-    data = np.random.randn(100)
+    
+    #Test with multiple properties
     plt.hist(data, bins=40, facecolor="red", edgecolor="blue", alpha=0.6)
     original = plt.xlabel("value", fontproperties="Comic Sans MS", weight="bold", size=16)
     changed = plt.xlabel("value", weight="bold", size=16, fontproperties="Comic Sans MS")
-    assert (original.get_size() == changed.get_size()) and (original.get_weight() == changed.get_weight()) and (original.get_family() == changed.get_family())
+    assert original.get_size() == changed.get_size() 
+    assert original.get_weight() == changed.get_weight()
+      
+    #Test on text update
+    text = txt.Text()
+    kwargs = {"fontproperties": "Times New Roman", "size": 30, "weight": "bold"}
+    text.update(kwargs)
+    assert text.get_size() == 30
+    assert text.get_weight() == "bold"
+    
+    #Acceptance Test
+    plt.hist(data, bins=40, facecolor="blue", edgecolor="black", alpha=0.5)
+    plt.xlabel("value", fontproperties='SimHei',size=20, color="red")
+    plt.ylabel("counts",size=20, color="red", fontproperties='SimHei')
+    plt.title("Test Graph", weight="bold", size=35, fontproperties="DejaVu Sans")
+    plt.show()
 
-def test_if_not_set_back_to_default_ylabell():
-    #Test that setting fontproperties doesn't change the other properties back to default
-    data = np.random.randn(100)
-    plt.hist(data, bins=40, facecolor="red", edgecolor="blue", alpha=0.6)
-    original = plt.ylabell("value", fontproperties="Comic Sans MS", weight="bold", size=16)
-    changed = plt.ylabell("value", weight="bold", size=16, fontproperties="Comic Sans MS")
-    assert (original.get_size() == changed.get_size()) and (original.get_weight() == changed.get_weight()) and (original.get_family() == changed.get_family())
+if __name__== "__main__":
+    test_kwargs_order()
+    print ("All tests passed")
